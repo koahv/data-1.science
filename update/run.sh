ebe5701829a62ec94dc806f756d4182e6c825b6d
Alexander <k0oahv@gmail.com>
2017-10-05 11:59:36 +0100

    update

--- update/run.sh
+++ update/run.sh
@@ -3,6 +3,10 @@ echo shell:locate.sh
 time sh locate.sh
 echo shell:index.sh
 time sh index.sh
+echo mv:words.txt
+mv words.txt ../word/
+echo shell:words.sh
+sh ../word/words.sh
 cd ..
 git pull
 git add .
