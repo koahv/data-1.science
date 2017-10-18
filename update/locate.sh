22f99ea3708557c09cc82ce5a53d097f581a099f
Alexander <k0oahv@gmail.com>
2017-10-18 14:47:17 +0100

    update

--- update/locate.sh
+++ update/locate.sh
@@ -1,6 +1,6 @@
 rm out.txt
 
-psql -U postgres -H -o out.txt -d ttrssdb2 -c "SELECT f.updated, g.title, f.title, f.link, d.title, d.site_url, d.feed_url, e.last_marked FROM ttrss_user_entries e INNER JOIN ttrss_feeds d ON d.id = e.feed_id INNER JOIN ttrss_entries f ON f.id = e.ref_id INNER JOIN ttrss_feed_categories g ON d.cat_id = g.id WHERE e.marked"
+psql -U postgres -H -o out.txt -d ttrssdb2 -c "SELECT f.updated, g.title, f.title, f.link, d.title, d.site_url, d.feed_url, f.author, e.last_marked FROM ttrss_user_entries e INNER JOIN ttrss_feeds d ON d.id = e.feed_id INNER JOIN ttrss_entries f ON f.id = e.ref_id INNER JOIN ttrss_feed_categories g ON d.cat_id = g.id WHERE e.marked"
 
 #psql -U postgres -H -o out.txt -d ttrssdb2 -c "SELECT f.updated, e.last_marked, g.title, f.title, f.link, d.title, d.site_url, d.feed_url FROM ttrss_user_entries e INNER JOIN ttrss_feeds d ON d.id = e.feed_id INNER JOIN ttrss_entries f ON f.id = e.ref_id INNER JOIN ttrss_feed_categories g ON d.cat_id = g.id WHERE e.marked"
 
