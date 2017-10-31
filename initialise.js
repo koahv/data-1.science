f234382a11ae4fea331cbd819ecdc6c10d8ed3c3
Alexander <k0oahv@gmail.com>
2017-10-31 09:46:52 +0000

    update

--- initialise.js
+++ initialise.js
@@ -14,8 +14,7 @@ function myFunction() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+  }
 
 
 	/*
