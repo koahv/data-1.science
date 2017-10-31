432d2a65c99df526abc615385aec019012091ffe
Alexander <k0oahv@gmail.com>
2017-10-31 09:26:08 +0000

    update

--- initialise.js
+++ initialise.js
@@ -6,6 +6,18 @@ $(document).ready( function () {
     } );
 } );
 
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+
 	/*
 	columnDefs: [ {
 		targets: [ 0 ],
