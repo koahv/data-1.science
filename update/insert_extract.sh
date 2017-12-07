ce93b673fb1ca297efdb48dd8c1783b59c93b305
Alexander <k0oahv@gmail.com>
2017-12-07 16:21:19 +0000

    update

--- update/insert_extract.sh
+++ update/insert_extract.sh
@@ -1,2 +1,15 @@
-psql -U postgres -d ttrssdb3_test -c \
-"INSERT INTO public.ttrss_entries (custom_extract) VALUES ('$today'";
+#Input file
+_db="extract.txt"
+
+if [[ -f "$_db" ]] # file exists
+then
+	# read file
+	while IFS='|' read -r id title factsheet
+	do
+	# echo $id
+	# echo $title
+	# echo $factsheet
+	psql -U postgres -d ttrssdb2 -c "UPDATE ttrss_entries SET custom_extract = '$factsheet' WHERE ID = $id RETURNING id, custom_extract;";
+	
+	done <"$_db"
+fi
