aee72c22b4fc16e15dcd9bce2a7cf62804b02f07
Alexander <k0oahv@gmail.com>
2018-02-28 12:59:33 +0000

    update

--- update/create_tag_page.sh
+++ update/create_tag_page.sh
@@ -6,48 +6,60 @@ psql -t -U postgres -o post_data/id.txt -d ttrssdb2 -c "SELECT f.id FROM ttrss_u
 
 sed -i '$ d' post_data/id.txt
 
-cp tags.md.bak tags.md
+#cp tags.md.bak tags.md
 rm taglist.txt
 
 # for each id
 while IFS=$'\n' read -r line_data; do # < post_data/id.txt
 
-echo $line_data
-
-	category=$(psql -X -A -U postgres -d ttrssdb2 --single-transaction --set ON_ERROR_STOP=on --no-align -t --quiet -c "SELECT g.title FROM ttrss_user_entries e INNER JOIN ttrss_feeds d ON d.id = e.feed_id INNER JOIN ttrss_entries f ON f.id = e.ref_id INNER JOIN ttrss_feed_categories g ON d.cat_id = g.id WHERE f.id = $line_data")
-	# echo $category
-
-	title=$(psql -X -A -U postgres -d ttrssdb2 --single-transaction --set ON_ERROR_STOP=on --no-align -t --quiet -c "SELECT f.title FROM ttrss_entries f WHERE f.id = $line_data")
-	# echo $title
-
-	link=$(psql -X -A -U postgres -d ttrssdb2 --single-transaction --set ON_ERROR_STOP=on --no-align -t --quiet -c "SELECT f.link FROM ttrss_entries f WHERE f.id = $line_data")
-	# echo $link
+	echo $line_data
 
 	tags=$(psql -X -A -U postgres -d ttrssdb2 --single-transaction --set ON_ERROR_STOP=on --no-align -t --quiet -c "SELECT e.tags_new FROM ttrss_user_entries e INNER JOIN ttrss_feeds d ON d.id = e.feed_id INNER JOIN ttrss_entries f ON f.id = e.ref_id INNER JOIN ttrss_feed_categories g ON d.cat_id = g.id WHERE f.id = $line_data")
 
-	echo $tags >> taglist.txt
+	echo $tags | tr , \\n  >> taglist.txt
+
+done < post_data/id.txt
+
 	
-	#mod_tags="$(echo -e ${tags})"
+# remove duplicates
+awk 'BEGIN{RS=ORS="\n"} !seen[$0]++' taglist.txt > taglist2.txt
+
+sort taglist2.txt > taglist_sorted.txt
+
+awk 'NF' taglist_sorted.txt > taglist_sorted2.txt
+
+rm taglist.txt taglist2.txt taglist_sorted.txt
+
+
+
+
+
+
+
+
 
 
 # replace new lines with comma
-mod_tags=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g' taglist.txt)
+#mod_tags=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g' taglist.txt)
 
-echo $mod_tags > taglist.txt
 
-# remove duplicates
-awk 'BEGIN{RS=ORS=","} !seen[$0]++' taglist.txt > taglist2.txt
+
+
+
+
+#echo $mod_tags | tr , \\n > sortedfile.txt
+
 
 # for each tag
-while IFS=$',' read -r tag; do # < taglist2.txt
+#while IFS=$',' read -r tag; do # < taglist2.txt
 
-#	echo "$tag " > tags.md
-
-
-	done < taglist2.txt
+	# echo "$tag " > tags.md
 
 
-done < post_data/id.txt
+#done < taglist2.txt
+
+
+
 
 
 #mod_tags2=`cat taglist.txt`
@@ -58,4 +70,11 @@ done < post_data/id.txt
 	
 #	echo $id >> id_tag.txt
 	
+#	category=$(psql -X -A -U postgres -d ttrssdb2 --single-transaction --set ON_ERROR_STOP=on --no-align -t --quiet -c "SELECT g.title FROM ttrss_user_entries e INNER JOIN ttrss_feeds d ON d.id = e.feed_id INNER JOIN ttrss_entries f ON f.id = e.ref_id INNER JOIN ttrss_feed_categories g ON d.cat_id = g.id WHERE f.id = $line_data")
+	# echo $category
 
+#	title=$(psql -X -A -U postgres -d ttrssdb2 --single-transaction --set ON_ERROR_STOP=on --no-align -t --quiet -c "SELECT f.title FROM ttrss_entries f WHERE f.id = $line_data")
+	# echo $title
+
+#	link=$(psql -X -A -U postgres -d ttrssdb2 --single-transaction --set ON_ERROR_STOP=on --no-align -t --quiet -c "SELECT f.link FROM ttrss_entries f WHERE f.id = $line_data")
+	# echo $link
